b83ad343f819f9902627abe4657455ca2aa4b9ef
r.dain24 <r.dain24@LAB108PC12>
2024-01-23 15:29:09 +0200

    pt.2

--- app2/MainWindow.xaml.cs
+++ app2/MainWindow.xaml.cs
@@ -27,19 +27,6 @@ namespace app2
         }
 
         string sqlCon = @"Data Source=LAB108PC12\SQLEXPRESS; Initial Catalog=LoginDB; Integrated Security=True;";
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            char[] arr = { '1', '~', '@' };
-            string ran = arr.ToString();
-            if (pswdBox.Password.Length < 8 && !pswdBox.Password.Contains(ran))
-            {
-                MessageBox.Show("Doesn't work");
-            }
-            else
-            {
-                MessageBox.Show($"Your usename is:{txtUsername.Text}, Password: {pswdBox.Password}      It worked Yay!!!!!!!");
-            }
-        }
 
         private void usernameClear(object sender, RoutedEventArgs e)
         {
@@ -62,6 +49,60 @@ namespace app2
                 //create the query 
 
                 string query = $"Insert into UserCredentials(Username,FirstName,LastName,Email,Password) values ('{txtUsername.Text}','{txtFirstname.Text}','{txtLastname.Text}','{txtEmail.Text}','{pswdBox.Password}')";
+                //establish the conn between the query and the db
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Success it works fine!");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(sqlCon);
+
+            try
+            {
+                //open the conneciton to the db 
+                conn.Open();
+
+                //create the query 
+
+                string query = $"Delete from UserCredentials Where Username = '{txtUsername.Text}'";
+                //establish the conn between the query and the db
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Success it works fine!");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(sqlCon);
+
+            try
+            {
+                //open the conneciton to the db 
+                conn.Open();
+
+                //create the query 
+
+                string query = $"Update UserCredentials SET FirstName = '{txtFirstname.Text}', LastName = '{txtFirstname.Text}', Email = '{txtEmail.Text}', Password = '{pswdBox.Password}' WHERE Username = '{txtUsername.Text}'";
                 //establish the conn between the query and the db
 
                 SqlCommand cmd = new SqlCommand(query, conn);
